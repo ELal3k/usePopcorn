d40de4189d70fa53d15490554bfee22b3c8751e8
Alexandros Polymeros <alpolymer@gmail.com>
2023-10-31 17:28:36 +0200

    app component tree

--- src/App.jsx
+++ src/App.jsx
@@ -54,6 +54,7 @@ const average = (arr) =>
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData)
+  const [watched, setWatched] = useState(tempWatchedData)
 
   return (
     <>
@@ -64,6 +65,10 @@ export default function App() {
       <Main>
         <Box>
           <MovieList movies={movies} />
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
         </Box>
       </Main>
     </>
@@ -119,29 +124,6 @@ function Movie({ movie }) {
         </p>
       </div>
     </li>
-  )
-}
-// ############# LISTBOX COMPONENTS END ################
-
-function WatchedBox() {
-  const [watched, setWatched] = useState(tempWatchedData)
-  const [isOpen2, setIsOpen2] = useState(true)
-
-  return (
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
-      >
-        {isOpen2 ? "–" : "+"}
-      </button>
-      {isOpen2 && (
-        <>
-          <WatchedSummary watched={watched} />
-          <WatchedMoviesList watched={watched} />
-        </>
-      )}
-    </div>
   )
 }
 
